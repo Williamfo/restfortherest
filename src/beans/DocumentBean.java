eabc51f4fdec79f3f50bebb573d379e9965cb7d5
William Olliso <will.olliso@gmail.com>
2015-06-10 12:44:25 +0200

    Several changes with some help from Florent.

--- src/beans/DocumentBean.java
+++ src/beans/DocumentBean.java
@@ -5,8 +5,22 @@ import javax.enterprise.inject.Model;
 @Model
 public class DocumentBean {
 
+	private String libelle;
 	public DocumentBean() {
 		
+	}
+	
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public String save() {
+		
+		return "ok";
 	}
 
 }
