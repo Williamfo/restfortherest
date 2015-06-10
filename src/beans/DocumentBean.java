0530ec2ecbd918ca8db3f5ed7ab0b16c115c5ac3
William Olliso <will.olliso2@gmail.com>
2015-06-10 11:42:47 +0200

    New Repository! restfortherest, a restful web service for the rest of us!

---
+++ src/beans/DocumentBean.java
@@ -0,0 +1,12 @@
+package beans;
+
+import javax.enterprise.inject.Model;
+
+@Model
+public class DocumentBean {
+
+	public DocumentBean() {
+		
+	}
+
+}
---
+++ src/restfortherest/Document.java
@@ -0,0 +1,71 @@
+package restfortherest;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+
+/**
+ * The persistent class for the document database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Document.findAll", query="SELECT d FROM Document d")
+public class Document implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+
+	private byte[] file;
+
+	private String libelle;
+
+	private BigDecimal x;
+
+	private BigDecimal y;
+
+	public Document() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public byte[] getFile() {
+		return this.file;
+	}
+
+	public void setFile(byte[] file) {
+		this.file = file;
+	}
+
+	public String getLibelle() {
+		return this.libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+	public BigDecimal getX() {
+		return this.x;
+	}
+
+	public void setX(BigDecimal x) {
+		this.x = x;
+	}
+
+	public BigDecimal getY() {
+		return this.y;
+	}
+
+	public void setY(BigDecimal y) {
+		this.y = y;
+	}
+
+}
